9e556988f454b0fbe41399c382b7370cc2609288
stevencodeblooded <steven.ochieng432@gmail.com>
2024-08-29 05:36:42 +0300

    Done

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
